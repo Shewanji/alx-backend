79620ec701af4abc0b18cb3c0d81b3ecc98a5c44
Shewanji <marrionwanjiru26@gmail.com>
2024-02-06 16:35:20 +0000

    Basic Babel setup

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""
+Basic Flask app
+"""
+
+from flask import Flask, render_template
+from flask_babel import Babel
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config:
+    """
+    Config class for Flask app
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+@app.route('/')
+def greet() -> str:
+    """
+    Renders index.html template
+
+    Returns:
+        str: Rendered HTML content
+    """
+    return render_template('1-index.html')
