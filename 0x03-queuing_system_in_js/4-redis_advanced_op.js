62c74ca68ef0a997be2ccb73b07fccc1541dbc77
Shewanji <marrionwanjiru26@gmail.com>
2024-03-05 20:19:15 +0000

    Node Redis client and advanced operations

---
+++ 0x03-queuing_system_in_js/4-redis_advanced_op.js
@@ -0,0 +1,32 @@
+import { createClient } from "redis";
+import { promisify } from "util";
+
+const redis = require('redis');
+
+const client = redis.createClient();
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+client.on('error', (err) => {
+  console.error(`Redis client not connected to the server: ${err.message}`);
+});
+
+function createHash() {
+  client.hset('HolbertonSchools', 'Portland', 50, redis.print);
+  client.hset('HolbertonSchools', 'Seattle', 80, redis.print);
+  client.hset('HolbertonSchools', 'New York', 20, redis.print);
+  client.hset('HolbertonSchools', 'Bogota', 20, redis.print);
+  client.hset('HolbertonSchools', 'Cali', 40, redis.print);
+  client.hset('HolbertonSchools', 'Paris', 2, redis.print);
+}
+
+function displayHash() {
+  client.hgetall('HolbertonSchools', (err, reply) => {
+    console.log(reply);
+  });
+}
+
+createHash();
+displayHash();
